fe56c4726ae9a6cd6320bc7c5803be3fc0accf2d
Lars Schöbitz <lars@lse.de>
2023-04-06 15:43:56 +0200

    Add raw data directory

---
+++ data-raw/data_processing.R
@@ -0,0 +1,18 @@
+# description -------------------------------------------------------------
+
+# R script to process uploaded raw data into a tidy dataframe
+
+# R packages --------------------------------------------------------------
+
+library(tidyverse)
+library(here)
+
+# read data ---------------------------------------------------------------
+
+
+# tidy data ---------------------------------------------------------------
+
+
+## code to prepare `DATASET` dataset goes here
+
+usethis::use_data(DATASET, overwrite = TRUE)
