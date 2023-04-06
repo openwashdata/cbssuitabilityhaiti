daad57b5317fce0809d30e982ac173d933d7e3a5
Lars Schöbitz <lars@lse.de>
2023-04-06 16:23:09 +0200

    Setup raw-data script and dictionary

--- data-raw/data_processing.R
+++ data-raw/data_processing.R
@@ -16,3 +16,11 @@ library(here)
 ## code to prepare `DATASET` dataset goes here
 
 usethis::use_data(DATASET, overwrite = TRUE)
+
+fs::dir_create(here::here("inst", "extdata"))
+
+write_csv(DATASET, here::here("inst", "extdata", "DATASET.csv"))
+
+openxlsx::write.xlsx(DATASET, here::here("inst", "extdata", "DATASET.xlsx"))
+
+
