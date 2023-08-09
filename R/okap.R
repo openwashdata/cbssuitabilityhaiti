b43f58f6aa95bc3cd2a6c9b627c7bd95291f4920
sebastian-loos <seba.loos@hotmail.com>
2023-08-09 17:18:51 +0200

    Update dictionary files

--- R/okap.R
+++ R/okap.R
@@ -10,19 +10,12 @@
 #'   \item{name}{Name of each nieghborhood unit}
 #'   \item{sup_km2}{Area of neighborhood in square km}
 #'   \item{cte}{Name of commune (administrative unit in Haiti)}
-#'   \item{qlty_water}{Unknown}
-#'   \item{qty_water}{Unknown}
-#'   \item{health_car}{Unknown}
-#'   \item{schooling}{Unknown}
-#'   \item{transport}{Unknown}
 #'   \item{economy}{Categorical socioeconomic status (low, medium)}
-#'   \item{ranking}{Unknown}
 #'   \item{sup_bati_km2}{area of neihborhood covered by buildings in square kilometers}
 #'   \item{density}{Categorical population density (least dense, somewhat dense, dense, very dense, most dense)}
 #'   \item{aptitude}{suitability of the site for a wastewater treatment system}
 #'   \item{zoning}{"group" if collective or grouped sanitation is possible in short term.}
 #'   \item{latrine}{Suggested pit latrine and septic allowance (allowed, not allowed)}
-#'   \item{standing}{Unknown}
 #'   \item{density_ra}{Catgoriccal population density according to the description of the variable "density" (values from 1 to 5)}
 #'   \item{economy_nu}{Categotical socioeconomic status according to the description of the variable "economy" (1=low, 2 = medium)}
 #'   \item{geometry}{Geospatial data of the neighborhood stored as a polygon}
