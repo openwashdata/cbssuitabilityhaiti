df495b35ccbbb5bd459a05bee6342258f6cc5c73
Lars Schöbitz <lars@lse.de>
2023-09-19 21:41:32 +0200

    Fix citation; add R-CMD-check

--- data-raw/package_processing.R
+++ data-raw/package_processing.R
@@ -191,3 +191,35 @@ usethis::use_pkgdown()
 
 # Build pkgdown website
 pkgdown::build_site()
+
+# install.packages("cffr")
+library(cffr)
+
+packageVersion("cffr")
+
+# Hard code doi
+# doi <- "10.5281/zenodo.6470427"
+
+# creates CFF with all author roles
+mod_cff <- cff_create("DESCRIPTION",
+                      dependencies = FALSE,
+                      keys = list(#"doi" = doi,
+                        "date-released" = Sys.Date()))
+
+# Remove the preferred-citation key
+mod_cff$`preferred-citation` <- NULL
+
+# writes the CFF file
+cff_write(mod_cff)
+
+# Now write a CITATION file from the CITATION.cff file
+# Use inst/CITATION instead (the default if not provided)
+path_cit <- file.path("inst/CITATION")
+
+write_citation("CITATION.cff", file = path_cit)
+
+# By last, read the citation
+cat(readLines(path_cit), sep = "\n")
+
+# Start of inst/CITATION
+utils::readCitationFile("inst/CITATION")
